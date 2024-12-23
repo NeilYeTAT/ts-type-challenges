6df543063f77dfbe05e3b6918234aeae38001efc
Ye Yu <nearjilt@gmail.com>
2024-12-23 19:51:16 +0800

    feat: todo *ye-awaited

---
+++ easy-challenges/*ye-awaited/index.js
@@ -0,0 +1,9 @@
+// 假如我们有一个 Promise 对象，这个 Promise 对象会返回一个类型。
+// 在 TS 中，我们用 Promise 中的 T 来描述这个 Promise 返回的类型。
+// 请你实现一个类型，可以获取这个类型。
+// 例如：Promise<ExampleType>，请你返回 ExampleType 类型。
+
+// If we have a type which is a wrapped type like Promise, how can we get the type which is inside the wrapped type?
+// For example: if we have Promise<ExampleType> how to get ExampleType?
+
+//todo
